308c08a5156d1050c70811b00ff9557b897094c3
Sunil <ysunilkumar2020@gmail.com>
2025-04-26 01:03:36 +0530

    profile page is created, but there is one issue at birthday icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomerView from './pages/CustomerView';
 import OrdersPage from './pages/OrdersPage';
 import AccountPage from './pages/AccountPage';
+import ProfilePage from './pages/ProfilePage';
 import './index.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<CustomerView />} />
           <Route path="/orders" element={<OrdersPage />} />
           <Route path="/account" element={<AccountPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </div>
     </Router>
